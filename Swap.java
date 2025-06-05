e881cbb9fe8231c75b12048044bd9afcaf2f69ae
prachi2314 <pa76190@gmail.com>
2025-06-05 12:57:11 +0530

    add swap file

---
+++ Swap.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+       int [] arr = {1, 3, 23, 9, 18};
+       reverse(arr);
+       System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+        int start =0;
+        int end = arr.length-1;
+        while (start<end) {
+            // reverse(arr,start, end);
+            start++;
+            end--;
+        }
+    }
+}
