18603c9152809d1de09568237d1305291e20f1a7
prachi2314 <pa76190@gmail.com>
2025-06-06 23:37:23 +0530

    add binar search file

---
+++ BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {6, 11, 15, 20, 23, 29, 31, 45};
+        int target = 14;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    // return index
+    // return -1 if doesn't exist
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end =  arr.length-1;
+        while(start<=end){
+            //  int mid = (start+end)/2;
+            int mid = start + (end-start)/2;
+
+            if(target < arr[mid]){
+                end = mid-1;
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
