b8df0a0d8bdcd43a1f71380f16c46c293a27a735
prachi2314 <pa76190@gmail.com>
2025-06-27 21:36:06 +0530

    add prime

---
+++ com/prachi/prime.java
@@ -0,0 +1,25 @@
+package com.prachi;
+
+public class prime {
+    public static void main(String[] args) {
+        int n = 20;
+        for(int i = 1; i <= n; i++){
+            System.out.println(i + " " + isPrime(i));
+        }
+    }
+
+    static boolean isPrime(int n){
+        if(n <= 2){
+            return false;
+        }
+        int c =2;
+        while (c*c <= n) {
+            if (n%2 == 0) {
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+
+}
